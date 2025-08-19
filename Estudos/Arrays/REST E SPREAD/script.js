dcf01f00eb5dcfd396bc46fefa7557f0c0a715c4
Johne Lima <johne.keyvid0@gmail.com>
2025-08-19 15:02:41 -0300

    Create script.js

---
+++ Estudos/Arrays/REST E SPREAD/script.js
@@ -0,0 +1,23 @@
+const valores = [1, 2, 3, 4, 5]
+const valores2 = [8, 7, 9, 12]
+
+const cliente = {
+    nome: 'Johne',
+    cpf: '506.278.628.29',
+    nascimento: '14/07/2003',
+    telefone: '11970798502',
+    endereco: {
+        rua: 'Tv David Eule',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        pais: 'Brasil',
+    }
+}
+
+// REST E SPREAD
+
+// PEGAR O NOME DO CLIENTE E NÃO DESCARTAR O RESTO
+
+// FUNÇÃO QUE SOMA N VALORES
+
+// CONCATENANDO ARRAYS
